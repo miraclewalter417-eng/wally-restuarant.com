66253ee07ffabb8fbe148e14cd4ea04cc3b0177c
miraclewalter417-eng <miraclewalter417@gmail.com>
2026-01-14 14:57:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// const headerMenuToggle = document.querySelector("#header_menu-toggle");
+// const headerMenus = document.querySelector("#header_menus");
+
+// headerMenuToggle.addEventListener("click", () => {
+//   headerMenus.classList.toggle("active"); 
+// });
